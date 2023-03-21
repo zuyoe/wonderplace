3ed5ec1a4d0f12ff956b4e5b468adce90932ccf3
zuyoe <9xtwo5@naver.com>
2023-03-21 20:25:52 +0900

    헤더 완료

--- js/script.js
+++ js/script.js
@@ -1,1 +1,23 @@
-window.onload = function () {};
+window.onload = function () {
+    // 모바일 메뉴 관련
+    const navMb = document.querySelector(".nav-mb");
+    const mbWrap = document.querySelector(".mb-wrap");
+    navMb.addEventListener("click", function () {
+        // 인라인 방식
+        // if (mbWrap.style.display === "block") {
+        //   mbWrap.style.display = "none";
+        // } else {
+        //   mbWrap.style.display = "block";
+        // }
+
+        // 클래스 추가 방식
+        const state = mbWrap.classList.contains("active");
+        if (state) {
+            navMb.classList.remoeve("active");
+            mbWrap.classList.remove("active");
+        } else {
+            navMb.classList.add("active");
+            mbWrap.classList.add("active");
+        }
+    });
+};
