b57627918efb65b05b0b048b5575ddb39d7c76e1
zuyoe <9xtwo5@naver.com>
2023-03-22 19:54:43 +0900

    비주얼 작업중

--- js/script.js
+++ js/script.js
@@ -13,11 +13,19 @@ window.onload = function () {
         // 클래스 추가 방식
         const state = mbWrap.classList.contains("active");
         if (state) {
-            navMb.classList.remoeve("active");
+            navMb.classList.remove("active");
             mbWrap.classList.remove("active");
         } else {
             navMb.classList.add("active");
             mbWrap.classList.add("active");
         }
+    });
+    // 비주얼 슬라이드
+    new Swiper(".swVisual", {
+        loop: true,
+        autoplay: {
+            delay: 1000,
+            disableOnInteraction: false,
+        },
     });
 };
