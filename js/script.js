2698c6077cc25104aab9cc52d4958aab8f80695d
zuyoe <9xtwo5@naver.com>
2023-03-27 20:09:23 +0900

    visual 슬라이드 작업중

--- js/script.js
+++ js/script.js
@@ -20,12 +20,69 @@ window.onload = function () {
             mbWrap.classList.add("active");
         }
     });
+
     // 비주얼 슬라이드
-    new Swiper(".swVisual", {
+    // 동적 즉, 서버에서 슬라이드를 생성하는 경우
+    const slideTotal = document.querySelectorAll(".swVisual .swiper-slide").length;
+    const slideControlUl = document.querySelector(".sw-visual-control");
+    let html = "";
+    for (let i = 0; i < slideTotal; i++) {
+        let tempI = i + 1;
+        if (i < 9) {
+            tempI = "0" + (i + 1);
+        }
+        html += `<li>${tempI}</li>`;
+    }
+    slideControlUl.innerHTML = html;
+
+    const swVisual = new Swiper(".swVisual", {
         loop: true,
+        speed: 800,
         autoplay: {
             delay: 1000,
             disableOnInteraction: false,
         },
+        navigation: {
+            nextEl: ".sw-visual-next",
+            prevEl: ".sw-visual-prev",
+        },
     });
+
+    // swVisual.on("slideChange", function () {
+    //   console.log("slide changed");
+    // });
+    swVisual.on("slideChangeTransitionStart", function () {
+        // console.log("slide changed", this.realIndex);
+        changeSwVisual(this.realIndex);
+    });
+
+    // 비주얼 슬라이드 제어 목록
+    let swVisualControlLi = document.querySelectorAll(".sw-visual-control li");
+    // console.log(swVisualControlLi);
+    swVisualControlLi.forEach((item, index) => {
+        // console.log(item);
+        item.addEventListener("click", function () {
+            // 내가 만드는 함수
+            changeSwVisual(index);
+        });
+    });
+
+    function changeSwVisual(_idx) {
+        // css 처리 (포커스 클래스 추가 및 제거)
+        // 1. active 클래스 모두 제거
+        swVisualControlLi.forEach((item, index) => {
+            item.classList.remove("active");
+        });
+        // 2. 클릭된 번호를 이용해서 클래스 추가하기
+        swVisualControlLi[_idx].classList.add("active");
+        // 3. 클릭된 번호를 이용해서 Swiper 를 이동
+        // * 주의 사항 (loop 를 옵션으로 준 경우는 아래 구문 금지)
+        // swVisual.slideTo(_index);
+        swVisual.slideToLoop(_idx, 500);
+    }
+
+        //  최초 클릭된 것 처럼 active 클래스 추가 
+    if (slideTotal > 0) {
+        changeSwVisual(0);
+    }
 };
